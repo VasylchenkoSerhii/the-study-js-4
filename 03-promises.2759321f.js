3d300bbedfdcf1102a7bf82d9516ce6759db8346
VasylchenkoSerhii <98521667+VasylchenkoSerhii@users.noreply.github.com>
2022-08-05 21:27:23 +0000

    Deploying to gh-pages from @ VasylchenkoSerhii/the-study-js-4@17d90acbfa40af893a39c215a8387ca9c1c67d25 🚀

--- 03-promises.2759321f.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('[name="delay"]'),document.querySelector('[name="step"]'),document.querySelector('[name="amount"]'),document.querySelector(".form__btn");
-//# sourceMappingURL=03-promises.2759321f.js.map
--- 03-promises.6ea4a06a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('[name="delay"]'),document.querySelector('[name="step"]'),document.querySelector('[name="amount"]'),document.querySelector(".form__btn");
-//# sourceMappingURL=03-promises.6ea4a06a.js.map
